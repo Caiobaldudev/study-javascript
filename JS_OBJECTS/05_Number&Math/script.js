152bd48995774cac1c0888b793a6ee810caa21a1
Caio Balduino <caiobalduinodev@hotmail.com>
2024-06-13 00:04:14 -0300

    Number & Math

--- JS_OBJECTS/05_Number&Math/script.js
+++ JS_OBJECTS/05_Number&Math/script.js
@@ -18,14 +18,14 @@ Number.isInteger(23.6); // false
 // NUMBER.PARSEFLOAT() E NUMBER.PARSEINT()
 // parseFloat() serve para retornarmos um número a partir de uma string. A String deve começar com um número. parseInt recebe também um segundo parâmetro que é o Radix, 10 é para decimal.
 
-parseFloat('99.50'); // Mesma função sem o Number
-Number.parseFloat('99.50'); // 99.5
-Number.parseFloat('100 Reais'); // 100
-Number.parseFloat('R$ 100'); // NaN
+parseFloat("99.50"); // Mesma função sem o Number
+Number.parseFloat("99.50"); // 99.5
+Number.parseFloat("100 Reais"); // 100
+Number.parseFloat("R$ 100"); // NaN
 
-parseInt('99.50', 10); // 99
+parseInt("99.50", 10); // 99
 parseInt(5.43434355555, 10); // 5
-Number.parseInt('100 Reais', 10); // 100
+Number.parseInt("100 Reais", 10); // 100
 
 // Float possui decimal, Integer não.
 
@@ -36,10 +36,10 @@ const preco2 = 2.99;
 prec2.toFixed(); // 3
 
 const carro = 1000.455;
-carro.toFixed(2) // 1000.46
+carro.toFixed(2); // 1000.46
 
 const preco3 = 1499.49;
-preco3.toFixed() // 1499
+preco3.toFixed(); // 1499
 
 // N.TOSTRING(RADIX)
 // Transforma o número em uma string com base no Radix. Use o 10 para o sistema decimal.
@@ -51,15 +51,15 @@ preco4.toString(10); // '2.99'
 // Formata o número de acordo com a língua e opções passadas.
 
 const preco = 59.49;
-preco.toLocaleString('en-US', {style: 'currency', currency: 'USD'}); // $59.49
-preco.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}); // R$ 59,49
+preco.toLocaleString("en-US", { style: "currency", currency: "USD" }); // $59.49
+preco.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }); // R$ 59,49
 
 // MATH
 // É um Objeto nativo que possui propriedades e métodos de expressões matemáticas comuns.
 
-Math.PI // 3.14159
-Math.E // 2.718
-Math.LN10 // 2.303
+Math.PI; // 3.14159
+Math.E; // 2.718
+Math.LN10; // 2.303
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
 
@@ -77,15 +77,15 @@ Math.round(4.3); // 4
 // MATH.MAX(), MATH.MIN() E MATH.RANDOM();
 // max() retorna o maior número de uma lista de argumentos, min() o menor número e random() um número aleatório entre 0 e 1.
 
-Math.max(5,3,10,42,2); // 42
-Math.min(5,3,10,42,2); // 2
+Math.max(5, 3, 10, 42, 2); // 42
+Math.min(5, 3, 10, 42, 2); // 2
 
 Math.random(); // 0.XXX
 Math.floor(Math.random() * 100); // entre 0 e 100
-Math.floor(Math.random() * 500);  // entre 0 e 500
+Math.floor(Math.random() * 500); // entre 0 e 500
 
 // Número random entre 72 e 32
-Math.floor(Math.random() * (72 - 32 + 1)) + 32; 
+Math.floor(Math.random() * (72 - 32 + 1)) + 32;
 Math.floor(Math.random() * (max - min + 1)) + min;
 
 // EXERCÍCIOS
@@ -96,27 +96,28 @@ const aleatorio = Math.floor(Math.random() * (2000 - 1050 + 1) + 1050);
 console.log(aleatorio);
 
 // Retorne o maior número da lista abaixo
-const numeros = '4, 5, 20, 8, 9';
+const numeros = "4, 5, 20, 8, 9";
 
-const arrayNumeros = numeros.split(', ')
-const numeroMaximo = Math.max(...arrayNumeros)
+const arrayNumeros = numeros.split(", ");
+const numeroMaximo = Math.max(...arrayNumeros);
 console.log(numeroMaximo);
 
 // Crie uma função para limpar os preços
 // e retornar os números com centavos arredondados
 // depois retorne a soma total
-const listaPrecos = ['R$ 59,99', ' R$ 100,222',
-                     'R$ 230  ', 'r$  200'];
+const listaPrecos = ["R$ 59,99", " R$ 100,222", "R$ 230  ", "r$  200"];
 
 function limparPreco(preco) {
-  preco = +preco.toUpperCase().replace('R$', '').trim().replace(',', '.');
-  preco = +preco.toFixed(2)
+  preco = +preco.toUpperCase().replace("R$", "").trim().replace(",", ".");
+  preco = +preco.toFixed(2);
   return preco;
 }
 
 let soma = 0;
 listaPrecos.forEach((preco) => {
-  soma += limparPreco(preco)
+  soma += limparPreco(preco);
 });
 
-console.log(soma.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}));
+console.log(
+  soma.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
+);
